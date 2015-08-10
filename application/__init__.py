922c61d7973db28ec34ba3a19b03635dd95bd35a
Colm Britton <colmjude@gmail.com>
2015-08-10 12:14:23 +0100

    add logging for heroku

--- application/__init__.py
+++ application/__init__.py
@@ -1,7 +1,15 @@
+import os
 from flask import Flask
 
 app = Flask(__name__)
 app.config.from_object('config')
+
+if os.environ.get('HEROKU') is not None:
+  import logging
+  stream_handler = logging.StreamHandler()
+  app.logger.addHandler(stream_handler)
+  app.logger.setLevel(logging.INFO)
+  app.logger.info('CSL startup')
 
 from application import views
 from application.assets import assets
