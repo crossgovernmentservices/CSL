6d9a122cd07bac17635628be2adea1305ece705f
Colm Britton <colmjude@gmail.com>
2015-09-25 16:08:56 +0100

    add js to make tabs work

--- application/rebrand/static/js/rebrand.js
+++ application/rebrand/static/js/rebrand.js
@@ -1,8 +1,42 @@
 ;(function($) {
   "use strict";
 
+  var tabNavSelector = ".activity-navigation .tabs",
+      tabContentClass = "tab-content",
+      $tabContentCol;
+
+  var switchTab = function( currentContentSel ) {
+    var currentContentSel = currentContentSel || $( tabNavSelector ).find( ".current" ).data( "tab-section");
+
+    // make correct tab current
+    $( tabNavSelector )
+      .find('a')
+      .removeClass("current")
+      .filter('[data-tab-section="'+ currentContentSel +'"]')
+        .addClass('current');
+
+    // show correct content
+    $tabContentCol
+      .hide()
+      .filter("#" + currentContentSel )
+        .show();
+
+    // still needs to update hash
+    // plus ARIA
+  };
+
   $(function() {
-    
+
+    // close all but current tab or value of hash
+    $tabContentCol = $( "." + tabContentClass );
+    var currentContent = (window.location.hash) ? window.location.hash.substring(1) : undefined;
+    switchTab(currentContent);
+
+    // bind event handler
+    $( tabNavSelector ).on('click', "a", function(){
+      switchTab( $(this).data("tab-section") );
+      return false;
+    });
   });
 
 }(jQuery));
