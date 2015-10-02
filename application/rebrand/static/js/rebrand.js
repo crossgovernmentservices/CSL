5fdf057080828a15113e92437d80c0bfe00972f2
Colm Britton <colmjude@gmail.com>
2015-10-02 17:56:02 +0100

    add flat tabs to schedule page, before your book and terms and conditions

--- application/rebrand/static/js/rebrand.js
+++ application/rebrand/static/js/rebrand.js
@@ -37,6 +37,17 @@
       switchTab( $(this).data("tab-section") );
       return false;
     });
+
+    // would cause issues if multiple flat-tab sections on page
+    var $tabPanes = $( ".tab-pane ");
+    $(".flat-tabs").on("click", "li", function() {
+      var $me = $( this );
+
+      $me.parents(".flat-tabs").find("li").removeClass("active").filter( $me ).addClass("active");
+      $tabPanes.hide().filter( "#" + $me.data("tab-pane" ) ).show();
+      return false;
+    });
+    $(".flat-tabs li.active").trigger("click");
   });
 
 }(jQuery));
